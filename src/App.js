b8b3b3f55f114deac428ba42f1f06a57d60eec72
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-08-24 13:35:25 +0200

    Todo functionality created

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ export default function App() {
   let [list, setList] = useState([]);
   let [input, setInput] = useState("");
 
-  return(
-  <div>
-    <h1>Todo List</h1>
-    <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-    <button>Add</button>
-  </div>
+  let addTodo = (todo) => {
+    let newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    setList([...list, newTodo]);
+
+    setInput("");
+  };
+
+  let deleteTodo = (id) => {
+    let newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
